44f02f8bab061c66a7fc0d8eda6b43942d4650ef
kriti-1-9 <dwivedikriti700@gmail.com>
2025-05-24 21:10:15 +0530

    ✅ Week 2: Updated packet sniffer to export packets to CSV

--- packetsniffing.py
+++ packetsniffing.py
@@ -1,4 +1,20 @@
-from scapy.all import sniff
+from scapy.all import sniff, IP
+import csv
+
+packets = sniff(count = 50)
+
+with open("packets.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Source IP", "Destination IP", "Protocol", "Length"])
+    
+    for pkt in packets:
+        if IP in pkt:
+            writer.writerow([
+                pkt[IP].src,
+                pkt[IP].dst,
+                pkt.proto,
+                len(pkt)
+            ])
 
 # Print a summary of 10 captured packets
-sniff(prn=lambda x: x.show(), count=10) # x.summary()
+# sniff(prn=lambda x: x.show(), count=10) # x.summary()
